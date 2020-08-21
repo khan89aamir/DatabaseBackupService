52be56df3fde0345660a827eec1ffb6ba7cf3401
AAMIR <AAMIR@KHAN>
2020-08-21 17:08:03 +0530

    Add project files.

---
+++ DatabaseBackupService/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ServiceProcess;
+using System.Windows.Forms;
+using System.Runtime.CompilerServices;
+[assembly: SuppressIldasm]
+
+namespace DatabaseBackupService
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        [Obsolete]
+        static void Main()
+        {
+            System.Diagnostics.Process[] p = System.Diagnostics.Process.GetProcessesByName("DatabaseBackupService");
+            if (p.Length > 1)
+            {
+                Application.Exit();
+            }
+            else
+            {
+                string myServiceName = System.Configuration.ConfigurationSettings.AppSettings["ServiceName"].ToString();
+                string status; //service status (For example, Running or Stopped)
+               
+                //display service status: For example, Running, Stopped, or Paused
+                ServiceController mySC = new ServiceController(myServiceName);
+                try
+                {
+                    status = mySC.Status.ToString();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Service not found. It is probably not installed. [exception=" + ex.Message + "]");
+                    return;
+                }
+                //display service status: For example, Running, Stopped, or Paused
+                //MessageBox.Show("Service status : " + status);
+
+                //if service is Stopped or StopPending, you can run it with the following code.
+                if (mySC.Status.Equals(ServiceControllerStatus.Stopped) | mySC.Status.Equals(ServiceControllerStatus.StopPending))
+                {
+                    try
+                    {
+                        mySC.Start();
+                        mySC.WaitForStatus(ServiceControllerStatus.Running);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error in starting the service: " + ex.Message);
+                    }
+                }
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new frmDatabackup());
+            }           
+        }
+    }
+}
---
+++ DatabaseBackupService/frmDatabackup.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using Microsoft.Win32;
+using CoreApp;
+//using System.Diagnostics;
+
+namespace DatabaseBackupService
+{
+    public partial class frmDatabackup : Form
+    {
+        clsConnection_DAL ObjDAL = new clsConnection_DAL(true);
+        clsUtility ObjUtil = new clsUtility();
+
+        RegistryKey rkApp;
+
+        string strBackUpPath = string.Empty;
+        string DatabaseName = string.Empty;
+        int hr;
+        int min;
+        DateTime backupTime;
+
+        public frmDatabackup()
+        {
+            InitializeComponent();
+            try
+            {
+                rkApp = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+                rkApp.SetValue("BackupService", Application.ExecutablePath.ToString());
+            }
+            catch { }
+        }
+
+        private void BackUpDatabase()
+        {
+            try
+            {
+                string sqlcmd = string.Empty;
+
+                sqlcmd = "BACKUP database " + DatabaseName + " TO DISK='" + strBackUpPath + @"\" + DatabaseName + ".bak' WITH INIT";
+                ObjDAL.ExecuteNonQuery(sqlcmd);
+                ObjDAL.UpdateColumnData("[Date]", SqlDbType.Date, DateTime.Now);
+                int a = ObjDAL.UpdateData(DatabaseName + ".dbo.BackupConfig", "1=1");
+                if (a > 0)
+                {
+                    WriteBackupLog(DatabaseName, "Auto Backup is Running\n BackUp Status : Backup Success");
+                }
+                else
+                {
+                    WriteBackupLog(DatabaseName, "Auto Backup is Running\n BackUp Status : Backup Faild");
+                }
+
+                System.Threading.Thread.Sleep(60000);
+            }
+            catch (Exception ex)
+            {
+                WriteBackupLog(DatabaseName, "Backup Faild\n Reason :" + ex.ToString());
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                this.Size = new Size(1, 1);
+                this.Hide();
+                if (File.Exists(Application.StartupPath + "\\AppConfig/ServerConfig.sc"))
+                {
+                    //string arr = ObjDAL.ReadConStringFromFile(Application.StartupPath + "\\AppConfig/ServerConfig.sc", true);
+                    //string[] a = arr.Split(new char[] { '=', ';' });
+                    DatabaseName = ObjDAL.GetCurrentDBName(true);
+                    WriteBackupLog(DatabaseName, "Auto Backup Service is running");
+
+                    LoadData();
+                    timer1.Enabled = true;
+                    timer1.Start();
+                }
+                else
+                {
+                    WriteBackupLog(System.Environment.MachineName, "File is not exist for Auto Backup Service");
+                    timer1.Stop();
+                    this.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                WriteBackupLog(DatabaseName, "Auto Backup service is closed \n Reason : " + ex.ToString());
+                timer1.Stop();
+                this.Close();
+            }
+        }
+
+        private void LoadData()
+        {
+            try
+            {
+                DataTable dt = ObjDAL.ExecuteSelectStatement("SELECT TOP 1 Path, CONVERT(date,Date) as Date, LTRIM(RIGHT(CONVERT(VARCHAR(20), Time, 100), 7)) AS Time FROM " + DatabaseName + ".dbo.BackupConfig WITH(NOLOCK)");
+                if (dt != null && dt.Rows.Count > 0)
+                {
+                    strBackUpPath = dt.Rows[0]["Path"].ToString();
+                    backupTime = Convert.ToDateTime(dt.Rows[0]["Time"]);
+                    hr = backupTime.Hour;
+                    min = backupTime.Minute;
+                }
+            }
+            catch
+            {
+                timer1.Stop();
+                this.Close();
+            }
+        }
+
+        private void WriteBackupLog(string dbName, string status)
+        {
+            ObjUtil.WriteToFile("Log Date : " + DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss tt"), Application.StartupPath + "\\backup.log", true);
+            ObjUtil.WriteToFile("Database Name : " + dbName, Application.StartupPath + "\\backup.log", true);
+            ObjUtil.WriteToFile("Status : " + status, Application.StartupPath + "\\backup.log", true);
+            ObjUtil.WriteToFile("______________________________________", Application.StartupPath + "\\backup.log", true);
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                if (DateTime.Now.Hour == hr && DateTime.Now.Minute == min)
+                {
+                    BackUpDatabase();
+                    timer1.Stop();
+                    this.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                WriteBackupLog(DatabaseName, "Error\n Reason : " + ex.ToString());
+                timer1.Stop();
+                this.Close();
+            }
+        }
+    }
+}
